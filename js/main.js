d74b9e5511565acea6dad634a5c7adfbcf6d1661
Ramachandran Rajagopal <ramo.phoenix7@gmail.com>
2021-05-13 21:59:15 +0530

    added partial changes in alphaWiseCountTest() function

--- js/main.js
+++ js/main.js
@@ -11,7 +11,13 @@ function sanitizeInput(name) {
 }
 
 function alphaWiseCount(name) {
-    // to be implemented.
+    let result = {};
+    name = sanitizeInput(name);
+    if (name) {
+        console.log("do something");
+        // logic to do it.
+    }
+    return result;
 }
 
 function alphaCount(name) {
@@ -39,9 +45,30 @@ function alphaCountTest(functionToTest) {
 }
 
 
+function alphaWiseCountTest(functionToTest) {
+    console.log("Testing method = " + functionToTest.name);
+    let assert = require('assert');
+    assert.deepStrictEqual(functionToTest(NaN), {});
+    assert.deepStrictEqual(functionToTest(""), {});
+    assert.deepStrictEqual(functionToTest("                      "), {});
+    assert.deepStrictEqual(functionToTest("***88???????!!!!##@#$@//!!"), {});
+    assert.deepStrictEqual(functionToTest("ramo"), {'r': 1, 'a': 1, 'm': 1, 'o': 1});
+    assert.deepStrictEqual(functionToTest("Ram,o"), {'r': 1, 'a': 1, 'm': 1, 'o': 1});
+    assert.deepStrictEqual(functionToTest("?ananth"), {'a': 2, 'n': 2, 't': 1, 'h': 1});
+    assert.deepStrictEqual(functionToTest("usha***"), {'u': 1, 's': 1, 'h': 1, 'a': 1});
+    // assert.deepStrictEqual(functionToTest("usha devi"), "ushadevi".length);
+    // assert.deepStrictEqual(functionToTest("ananth      murugan"), "ananthmurugan".length);
+    // assert.deepStrictEqual(functionToTest("abc def xyz"), "abcdefxyz".length);
+    // assert.deepStrictEqual(functionToTest("   usha devi"), "ushadevi".length);
+    // assert.deepStrictEqual(functionToTest("anan****th      murug//an$$$"), "ananthmurugan".length);
+    // assert.deepStrictEqual(functionToTest("$$$$abc def xyzxx****9(((("), "abcdefxyzxx".length);
+    console.log("All Test passes successfully!");    
+}
+
 function flames(name1, name2) {
     return "F - L - A - M - E - S";
 }
 
 alphaCountTest(alphaCount);
+alphaWiseCountTest(alphaWiseCount);
 
